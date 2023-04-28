6df90867953d7cb848fb65af1b51ef9402d5a2aa
infermi <80839246+infermi@users.noreply.github.com>
2023-04-28 18:20:26 +0300

    added an orginized pallate and changed a lot

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,23 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
         fontFamily: {
-            'sans': '"Open Sans"',
-            'heebo': '"Heebo"',
-            'varela': '"Varela Round"',
-            'fredo': '"Fredoka"'
-
+            sans: '"Open Sans"',
+            heebo: '"Heebo"',
+            varela: '"Varela Round"',
+            fredo: '"Fredoka"',
         },
         extend: {
             screens: {
-                'xs': '380px'
+                xs: "380px",
             },
-            backgroundImage: { 'profile': 'url("public/me.jpg")' },
-            colors: { 'pone': '#FDF0CCff', 'ptwo': '#005863ff', 'pthree': '#0A1C25ff', 'pfour': '#431C2Eff', 'pfive': '#FBAD53ff', 'psix': '#D3547Aff' }
+            colors: {
+                dark: "#121820",
+                bluegreen: "#0E404Bff",
+                apricot: "#E5906F",
+                wedgewood: "#467699",
+                easternblue: "#28A9AA",
+                cta: "#FAAB55ff"
+            },
         },
     },
-    plugins: [
-        require('tailwind-scrollbar-hide')
-    ],
-}
+    plugins: [require("tailwind-scrollbar-hide")],
+};
